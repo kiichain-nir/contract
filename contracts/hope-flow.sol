b318a25fa165b97e2383d01c2b6d53d77fcacd1d
anupamakoirala <anukoirala.96@hotmail.com>
2024-07-31 09:49:33 +0545

    Revert "contract initial updates"
    This reverts commit c685255aa5383728b525af994ccaadd202818a08.

--- contracts/hope-flow.sol
+++ contracts/hope-flow.sol
@@ -20,56 +20,47 @@ contract ProjectManager {
 
     Project[] public projects;
 
-    mapping(address =>uint256) projectToken;
-
     event ProjectCreated(string name, string symbol, address tokenAddress);
 
-    event BeneficiaryAdded(address indexed tokenAddress,address  indexed benAddress,uint256 amount);
+    function createProject(string memory name, string memory tokenName, string memory symbol) external {
+        ProjectToken token = new ProjectToken(tokenName, symbol, address(this));
 
-    function createProject(string memory name, string memory tokenName, string memory symbol) external returns(uint256 length){
-        ProjectToken token = new ProjectToken(tokenName, symbol);
-        uint256 index = projects.length ;
+        token.mint(msg.sender, 100000 * 10 ** token.decimals());
 
-        token.mint(address(this), 100000 * 10);
-       address tokenAddress = address(token);
-       projects.push();
-        Project storage project = projects[index];
-        project.name =name;
-        project.tokenSymbol = symbol;
-        project.tokenName = tokenName;
-        project.tokenAddress = tokenAddress;
+        projects.push(Project({
+            name: name,
+            tokenSymbol: symbol,
+            tokenName: tokenName,
+            tokenAddress: address(token),
+            beneficiaries: []
+        }));
 
         emit ProjectCreated(name, symbol, address(token));
-
-        return index;
     }
 
     function addBeneficiary(address tokenAddress, address walletAddress, uint64 amount) external {
-        uint256 projectLength = projects.length;
-
-        uint256 projectId = projectToken[tokenAddress];
-        require(projectLength > 0,"Project not created");
-        Project storage project = projects[projectId];
-        project.beneficiaries.push(Beneficiary({
-            walletAddress:walletAddress,
-            amount:amount
-        }));
+        bool projectFound = false;
+        for (uint i = 0; i < projects.length; i++) {
+            if (projects[i].tokenAddress == tokenAddress) {
+                projects[i].beneficiaries.push(Beneficiary({
+                    walletAddress: walletAddress,
+                    amount: amount
+                }));
+                projectFound = true;
+                emit BeneficiaryAdded(tokenAddress, walletAddress, amount);
+                break;
+            }
+        }
+        require(projectFound, "Project not found");
     }
 
     function getProject(uint256 index) external view returns (string memory, string memory, address) {
         require(index < projects.length, "Project does not exist");
         Project storage project = projects[index];
-        return (project.name, project.tokenSymbol, project.tokenAddress);
+        return (project.name, project.symbol, project.tokenAddress);
     }
 
     function getAllProjects() external view returns (Project[] memory) {
         return projects;
     }
-
-    function getBeneficiaries(uint256 projectIndex) external view returns(Beneficiary[] memory benDetails){
-          benDetails = projects[projectIndex].beneficiaries;
-          return benDetails;
-    }
-
-    
 }
